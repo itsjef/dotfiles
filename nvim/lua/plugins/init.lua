4c211ab6b9440fa463ef902be25bd19641fac8ff
Tran Duc Anh <anh.tran@parcelperform.com>
2025-04-16 11:32:45 +0700

    [nvim] add avante.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -157,11 +157,62 @@ return {
   },
 
   -- AI assistants
-  -- {
-  --   'Exafunction/codeium.nvim',
-  --   event = 'InsertEnter',
-  --   config = function()
-  --     require('codeium').setup({})
-  --   end
-  -- },
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    version = false,
+    opts = {
+      -- provider = 'openai',
+      -- openai = {
+      --   endpoint = 'https://api.openai.com/v1',
+      --   model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
+      --   timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+      --   temperature = 0,
+      --   max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+      --   --reasoning_effort = 'medium', -- low|medium|high, only used for reasoning models
+      -- },
+      provider = 'claude',
+      claude = {
+        endpoint = 'https://api.anthropic.com',
+        -- model = 'claude-3-5-sonnet-20241022',
+        model = 'claude-3-7-sonnet-20250219',
+        timeout = 30000, -- Timeout in milliseconds
+        temperature = 0,
+        max_tokens = 4096,
+        disable_tools = true, -- disable tools!
+      },
+      behaviour = {
+        enable_claude_text_editor_tool_mode = true,
+      }
+    },
+    build = 'make',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+          },
+        },
+      },
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+  }
 }
