cfa683da9282f1092ddd80b436c58375ad846341
Tran Duc Anh <anh.tran@parcelperform.com>
2025-07-31 17:27:48 +0700

    [nvim] update lualine.nvim config

--- nvim/lua/pluginconfigs.lua
+++ nvim/lua/pluginconfigs.lua
@@ -101,17 +101,10 @@ require('lualine').setup {
     lualine_b = { 'branch' },
     lualine_c = { { 'filename', path = 1 } },
     lualine_x = { 'copilot', 'encoding', 'fileformat', 'filetype' },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location' }
+    lualine_y = { 'progress', 'location' },
+    lualine_z = { {'datetime', style = '%H:%M'} }
   },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { { 'filename', path = 1 } },
-    lualine_x = { 'location' },
-    lualine_y = {},
-    lualine_z = {}
-  },
+  inactive_sections = {},
   tabline = {},
   extensions = {}
 }
