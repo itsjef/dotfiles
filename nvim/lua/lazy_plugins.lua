0fecd32aa3de34627a27ca72aee2c6fe5f3847f2
Tran Duc Anh <anh.tran@parcelperform.com>
2025-10-23 10:45:46 +0700

    [nvim] remove nvim-treesitter-textobjects

--- nvim/lua/lazy_plugins.lua
+++ nvim/lua/lazy_plugins.lua
@@ -91,7 +91,6 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
       'nvim-treesitter/nvim-treesitter-context',
       'HiPhish/rainbow-delimiters.nvim',
     },
