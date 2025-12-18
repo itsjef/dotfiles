6a8f8aeea2b7754a28020df0af8a771cf8ef6fb0
Tran Duc Anh <anh.tran@parcelperform.com>
2025-12-19 04:41:39 +0700

    [nvim] remove duplicated git-related plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -17,20 +17,6 @@ return {
     },
   },
   {
-    'NeogitOrg/neogit',
-    dependencies = { 'sindrets/diffview.nvim' },
-  },
-  {
-    'lewis6991/gitsigns.nvim',
-    config = function()
-      require('gitsigns').setup {
-        on_attach = function(bufnr)
-          require('keymappings').gitsigns_keys(bufnr)
-        end
-      }
-    end
-  },
-  {
     'MeanderingProgrammer/render-markdown.nvim',
     opts = {},
   },
