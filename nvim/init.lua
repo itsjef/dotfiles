875fe908df2ab1ee40ea2e1860f42e813ebbebac
Tran Duc Anh <anh.tran@parcelperform.com>
2026-04-09 11:46:12 +0700

    [nvim] enabled ui2, remove noice.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,9 @@
 vim.g.mapleader = ','
 vim.g.maplocalleader = ','
 
+-- enable ui2
+require('vim._core.ui2').enable({})
+
 -- plugins with lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
